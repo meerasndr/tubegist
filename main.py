15a5a2848f937de4f4f36029a3dc6446ac177354
meerasndr <mirage.sundial@gmail.com>
2023-05-29 11:26:03 +0530

    put logic behind FastAPI endpoint

--- main.py
+++ main.py
@@ -10,6 +10,35 @@ from langchain import LLMChain
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this list based on your requirements
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+	return {"message": "Hello"}
+@app.post("/video_url/")
+async def send_url(url:str):
+	video_title, video_id = get_title_id(url)	
+	template = "Can you summarize {this}?"
+	llm = OpenAI(model_name="text-davinci-003")
+	prompt = PromptTemplate(template=template, input_variables=['this'])
+	chain = LLMChain(llm=llm, prompt=prompt)
+	input = {'this': get_transcript(video_id, video_title)}
+	try:
+		return {"message" : chain.run(input)}
+	except:
+		return {"message": "There's an unforeseen error. \nA likely cause is the video length being high. \nWe are working on supporting this. Thanks for your patience. "}
+
 def get_title_id(url):
 	options = {}
 	try:
@@ -18,34 +47,18 @@ def get_title_id(url):
 			video_title = info_dict.get('title', None)
 			video_id = info_dict.get('id', None)
 	except:
-		print("Enter a valid YouTube URL")
+		return "Enter a valid YouTube URL"
 	return [video_title, video_id]
 
-def get_transcript(video_id):
+def get_transcript(video_id, video_title):
 	try: 
 		translist = YouTubeTranscriptApi.get_transcript(video_id)
 		transcript_text = video_title
 		for item in translist:
 			transcript_text += item['text']
 	except:
-		print("This YouTube video does not have a transcript. It is probably a music video or uses a non-English language.")
-		print("Goodbye!")
-		exit(0)
+		return "This YouTube video does not have a transcript. It is probably a music video or uses a non-English language."
 	return transcript_text
 
-
-try:
-	video_title, video_id = get_title_id(sys.argv[1])
-except:
-	print("Enter the YouTube video URL as an argument: \n `python main.py [video_url]`")
-	exit(0)
-template = "Can you summarize {this}?"
-llm = OpenAI(model_name="text-davinci-003")
-prompt = PromptTemplate(template=template, input_variables=['this'])
-chain = LLMChain(llm=llm, prompt=prompt)
-input = {'this': get_transcript(video_id)}
-try:
-	print(chain.run(input))
-except:
-	print("There's an unforeseen error. \nA likely cause is the video length being high. \nWe are working on supporting this. Thanks for your patience. ")
-	exit(0)
+if __name__ == "__main__":
+	uvicorn.run(app, host="0.0.0.0", port=8000)
